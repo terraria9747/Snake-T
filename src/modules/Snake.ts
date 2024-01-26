b21f7dd90e214432ec8779a422a1a16d6b5c2e18
范志鹏2002 <2710557309@qq.com>
2024-01-26 23:06:51 +0800

    蛇的相关信息

---
+++ src/modules/Snake.ts
@@ -0,0 +1,44 @@
+// Snake
+
+class Snake {
+	// 蛇头
+	head: HTMLElement;
+	// 蛇身体
+	bodies: HTMLCollection;
+	// 蛇信息
+	element: HTMLElement;
+
+	constructor() {
+		// 获取蛇的信息
+		this.element = document.getElementById("snake")!
+		// as HTMLElement 类型断言
+		this.head = document.querySelector("#snake > div")! as HTMLElement;
+		this.bodies = this.element.getElementsByTagName("div");
+	}
+
+	// 获取蛇头的坐标
+	get X() {
+		return this.head.offsetLeft;
+	}
+
+	get Y() {
+		return this.head.offsetTop;
+	}
+
+	// 设置蛇头坐标
+	set X(value: number) {
+		this.head.style.left = value + "px"
+	}
+	
+	set Y(value: number) {
+		this.head.style.top = value + "px"
+	}
+
+	// 增加蛇的身体
+	addBody() {
+		// 向element中添加一个div
+		this.element.insertAdjacentHTML("beforeend", "<div></div>")
+	}
+}
+
+export default Snake
